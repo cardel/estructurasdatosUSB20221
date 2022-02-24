2664e72b5cdcb5618728d61fc0106fdc0e7a4d38
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2022-02-24 15:31:18 -0500

    Codigos 24 de Feb

--- corte1/tema1ordenamiento/ordenamiento.py
+++ corte1/tema1ordenamiento/ordenamiento.py
@@ -7,9 +7,6 @@ def ordenar_ingenuo(arreglo):
             if arreglo[j]<arreglo[i]:
                 arreglo[i],arreglo[j] = arreglo[j],arreglo[i]
 
-arreglo1 = np.array([4,2,1,0,3,5,5,5,1,1,6])
-ordenar_ingenuo(arreglo1)
-print(arreglo1)
 
 def insertion_sort(arreglo):
     for j in range(2, arreglo.shape[0]+1):
@@ -20,36 +17,77 @@ def insertion_sort(arreglo):
             i = i-1
         arreglo[i] = key 
 
-arreglo1 = np.array([4,2,1,0,3,5,5,5,1,1,6])
-insertion_sort(arreglo1)
-print(arreglo1)
+def selectionSort(arreglo):
+    for i in range(0,arreglo.shape[0]):
+        posMenor = i
+        valorMenor = arreglo[i]
+        for j in range(i+1,arreglo.shape[0]):
+            if arreglo[j]<valorMenor:
+                posMenor = j
+                valorMenor = arreglo[j]
+        arreglo[i],arreglo[posMenor] = arreglo[posMenor],arreglo[i]
 
+
+
+def ordenamiento_burbuja(arreglo):
+    for k in range(0,arreglo.shape[0]): #Recortar la ultima posición
+        for i in range(0,arreglo.shape[0]-1-k): #-1 desborde, -k porque en cada iteración tengo el ultimo elemento ordenado
+            if arreglo[i]>arreglo[i+1]:
+                arreglo[i],arreglo[i+1] = arreglo[i+1],arreglo[i]
+    
 
 
 tiemposOI = []
 tiemposIS = []
-tamanios = [100,1000,5000,7500]
+tiemposSC = []
+tiemposOB = []
+tamanios = [50,100,150,200,250,300,350]
 for n in tamanios:
-    arregloX = np.random.randint(0,100000,(n))
-    ini = time.time()
-    ordenar_ingenuo(arregloX)
-    fin = time.time()
-    tiemposOI.append(fin-ini)
+    tIn = 0
+    tIs = 0
+    tSc = 0
+    tOb = 0
+    experimentos = 100
+    for i in range(0,experimentos):
 
-    arregloX = np.random.randint(0,100000,(n))
-    ini = time.time()
-    insertion_sort(arregloX)
-    fin = time.time()
-    tiemposIS.append(fin-ini)
+        arregloX = np.random.randint(0,100000,(n))
+        ini = time.time()
+        ordenar_ingenuo(arregloX)
+        fin = time.time()
+        tIn += (fin-ini)
 
-print(tiemposOI)
-print(tiemposIS)
+        arregloX = np.random.randint(0,100000,(n))
+        ini = time.time()
+        insertion_sort(arregloX)
+        fin = time.time()
+        tIs+=(fin-ini)
+
+        arregloX = np.random.randint(0,100000,(n))
+        ini = time.time()
+        selectionSort(arregloX)
+        fin = time.time()
+        tSc+=(fin-ini)
+
+        arregloX = np.random.randint(0,100000,(n))
+        ini = time.time()
+        ordenamiento_burbuja(arregloX)
+        fin = time.time()
+        tOb+=(fin-ini)
+
+
+    tiemposOI.append(tIn/experimentos)
+    tiemposIS.append(tIs/experimentos)
+    tiemposSC.append(tSc/experimentos)
+    tiemposOB.append(tOb/experimentos)
 
 fig = plt.figure(dpi=300)
 plt.plot(tamanios,tiemposOI,"r-", label="Algoritmo ingenuo")
 plt.plot(tamanios,tiemposIS,"b-",label="Insertion Sort")
+plt.plot(tamanios,tiemposSC,"g-",label="Selection Sort")
+plt.plot(tamanios,tiemposOB,"y-",label="Bubble Sort")
+
 plt.xlabel("Tamaño")
 plt.ylabel("Segundos")
 plt.legend()
 plt.tight_layout()
-plt.show()|
+plt.show()
